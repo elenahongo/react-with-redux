db0a7664bbbedc730c911391ecdeae288e5ecb17
Elena Hernandez Vazquez <selenelunaw@hotmail.com>
2020-03-27 13:15:58 -0600

    two state properties working and delete option available

--- src/App.js
+++ src/App.js
@@ -11,6 +11,20 @@ class App extends Component {
         </div>
         <button onClick={this.props.onAgeUp}>Age UP</button>
         <button onClick={this.props.onAgeDown}>Age Down</button>
+        <hr />
+        <div>History</div>
+        <div>
+          <ul>
+            {this.props.history.map(element => (
+              <li
+                key={element.id}
+                onClick={() => this.props.onDelItem(element.id)}
+              >
+                {element.age}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
@@ -18,17 +32,16 @@ class App extends Component {
 
 const mapStateToProps = state => {
   return {
-    age: state.age
+    age: state.age,
+    history: state.history
   };
 };
 
 const mapDispachToProps = dispatch => {
   return {
-    onAgeUp: () => dispatch({ type: "AGE_UP", value: 1 }),
-    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 1 })
+    onAgeUp: () => dispatch({ type: "AGE_UP", value: 10 }),
+    onAgeDown: () => dispatch({ type: "AGE_DOWN", value: 10 }),
+    onDelItem: (id) => dispatch({ type: "DEL_ITEM", id: id })
   };
 };
-export default connect(
-  mapStateToProps,
-  mapDispachToProps
-)(App);
+export default connect(mapStateToProps, mapDispachToProps)(App);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,5 +1,6 @@
 const initialState = {
-    age:20
+    age:20,
+    history: []
 };
 
 const reducer = (state=initialState, action) => {
@@ -7,11 +8,30 @@ const reducer = (state=initialState, action) => {
 
     switch(action.type){
         case 'AGE_UP': 
-            newState.age += action.value;
+            //newState.age += action.value;
+            console.log(state.history)
+            return {
+                ...state,
+                age: state.age + action.value,
+                history: state.history.concat({id:Math.random(), age: state.age + action.value})
+            }
             break;
         
         case 'AGE_DOWN': 
-            newState.age -= action.value;
+            //newState.age -= action.value;
+            return {
+                ...state,
+                age: state.age - action.value,
+                history: state.history.concat({id:Math.random(), age: state.age - action.value})
+            };
+            break;
+
+        case 'DEL_ITEM':
+            console.log("fue llamado el array" + action.id)
+            return {
+                ...state,
+                history: newState.history.filter( (el) => el.id !== action.id)
+            }
             break;
     }
     return newState;
